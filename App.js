8beee7c6b2f7d886eb87cde342e4adf366c0b32e
Daven Moonesawmy <davemoon2004@gmail.com>
2025-08-12 17:59:31 +0100

    controls implementation initial

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useState, useEffect } from "react";
+
+const Tile = ({ tilePos }) => {
+  return (
+    <View
+      style={[styles.tile, { top: tilePos.top, left: tilePos.left }]}
+    ></View>
+  );
+};
 
 export default function App() {
   const [gameboard, setGameboard] = useState([]);
+  const [tilePos, setTilePos] = useState({ top: 0, left: 0 });
 
   useEffect(() => {
     const initializeBoard = () => {
@@ -24,6 +33,33 @@ export default function App() {
             <Text>{el.loc}</Text>
           </View>
         ))}
+        <Tile tilePos={tilePos} />
+      </View>
+      <View style={styles.controls}>
+        <Button
+          style={styles.controlButton}
+          title="Left"
+          onPress={() => {
+            console.log("Button pressed!");
+            setTilePos((tilePos) => ({ ...tilePos, left: tilePos.left - 80 }));
+          }}
+        />
+        <Button
+          style={styles.controlButton}
+          title="Down"
+          onPress={() => {
+            console.log("Button pressed!");
+            setTilePos((tilePos) => ({ ...tilePos, top: tilePos.top + 80 }));
+          }}
+        />
+        <Button
+          style={styles.controlButton}
+          title="Right"
+          onPress={() => {
+            console.log("Button pressed!");
+            setTilePos((tilePos) => ({ ...tilePos, left: tilePos.left + 80 }));
+          }}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -35,12 +71,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "gray",
   },
   board: {
-    flex: 1,
+    flex: 2,
     flexDirection: "row",
     flexWrap: "wrap",
     width: 3 * 80,
+    height: 5 * 80,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
@@ -52,5 +90,21 @@ const styles = StyleSheet.create({
     height: 80,
     justifyContent: "center",
     alignItems: "center",
+  },
+  controls: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  controlButton: {
+    width: 50,
+    height: 50,
+    flex: 2,
+    margin: 20,
+  },
+  tile: {
+    width: 80,
+    height: 80,
+    backgroundColor: "purple",
+    position: "absolute",
   },
 });
