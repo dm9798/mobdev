a1e7c4780d7fdffdc48e0a20871de7acd2e5382c
Daven Moonesawmy <davemoon2004@gmail.com>
2025-08-12 14:34:32 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [gameboard, setGameboard] = useState([]);
+
+  useEffect(() => {
+    const initializeBoard = () => {
+      let initialArr = [];
+      for (let i = 0; i < 15; i++) {
+        initialArr.push({ loc: i });
+      }
+      setGameboard(initialArr);
+    };
+    initializeBoard();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.board}>
+        {gameboard.map((el, index) => (
+          <View key={index} style={styles.cell}>
+            <Text>{el.loc}</Text>
+          </View>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  board: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: 3 * 80,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cell: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: 80,
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
